6ec1c04fdaaf54c42f235c8fbadcab6f6847c203
Alexey-D <81498958+Alexey-D@users.noreply.github.com>
2021-04-02 03:28:31 +0300

    Add files via upload

--- Полярная СК/Полярная_СК.cs
+++ Полярная СК/Полярная_СК.cs
@@ -12,26 +12,31 @@ namespace Полярная_СК
 {
     public partial class  Полярная_СК : Form
     {
+        // Опредление начальных значений некоторых элементов (истинность селекторов "+" и "sin") 
         public Полярная_СК()
         {
             InitializeComponent();
             Plus.Checked = true;
             Sin.Checked = true;
         }
+        // Обработка уравнения
         public void Ok_Click(object sender, EventArgs e)
         {
-            Chart.Series[0].Points.Clear();
+            Chart.Series[0].Points.Clear(); // Очистка графика
             double x, y, res;
             int p, num1, num2;
+            // Цикл для обработки каждого возможного угла
             for (int angle = 0; angle <= 360; angle++)
             {
-                p = Int32.Parse(P.Text);
-                num1 = Int32.Parse(Num1.Text);
-                num2 = Int32.Parse(Num2.Text);
+                p = Int32.Parse(P.Text); // Общий множитель
+                num1 = Int32.Parse(Num1.Text); // Число, из которого отнимают или к которому прибавляют
+                num2 = Int32.Parse(Num2.Text); // Множитель угла
+                // Проверка селекторов "sin" и "cos"
                 if (Sin.Checked)
                 {
+                    // Проверка селекторов "+" и "-"
                     if (Plus.Checked)
-                        res = p * (num1 + Math.Sin(num2 * angle));
+                        res = p * (num1 + Math.Sin(num2 * angle)); // Резултат уравнения
                     else
                         res = p * (num1 - Math.Sin(num2 * angle));
                 }
@@ -42,9 +47,10 @@ namespace Полярная_СК
                     else
                         res = p * (num1 - Math.Cos(num2 * angle));
                 }
+                // Перевод в декартову систему (x и y)
                 x = res * Math.Cos(angle);
                 y = res * Math.Sin(angle);
-                Chart.Series[0].Points.AddXY(x, y);
+                Chart.Series[0].Points.AddXY(x, y); // Добавление точек на график
             }
         }
     }
